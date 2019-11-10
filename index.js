5d7213b6967e27e394404a196704accb7d1163cd
Shaun Lusk <shaun.lusk@gmail.com>
2019-11-10 17:07:55 -0600

    Work in progress. able to save users to db'

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const snoowrap = require('snoowrap');
 const CarubsDB = require('./CarubsDB');
 const config = require('./config');
 const async = require('async');
+const PromiseEachHelper = require('./PromiseEachHelper');
 
 // Create a new snoowrap requester with OAuth credentials.
 // For more information on getting credentials, see here: https://github.com/not-an-aardvark/reddit-oauth-helper
@@ -17,115 +18,41 @@ var r = new snoowrap({
 const userSet = {};
 let threadProcessedCount = 0;
 let userProcessedCount = 0;
-// const carubsDb = new CarubsDB();
+const carubsDb = new CarubsDB(config.dbPath);
 
-
-// function threadProcessed(totalThreadCount) {
-//   threadProcessedCount++;
-//   if (threadProcessedCount === totalThreadCount) {
-
-//     populateUserComments();
-//   }
-// }
-
-// function userProcessed(totalUserCount) {
-//   userProcessedCount++;
-//   if (userProcessedCount === totalUserCount) {
-//     // console.log(db);
-//     var users = Object.keys(db);
-//     // console.log(users[0], db[users[0]].comments[0]);
-//   }
-// }
-
-// function populateUserComments() {
-//   const users = Object.keys(db);
-//   users.forEach((user, idx, users) => {
-//     console.log("Processing user ", user, ", " , idx, "/", users.length);
-//     carubsDb.insertUser(user, (err) =>{
-//       r.getUser(user).getComments({sort:'top', limit:50}).then((comments) => {
-//         console.log("Found ", comments.length, " comments for ", user);
-//         comments.forEach((comment) => {
-//           db[user].comments.push({
-//             id:comment.id,
-//             user_id:user,
-//             created_utc: comment.created_utc,
-//             body: comment.body,
-//             ups: comment.ups,
-//             downs: comment.downs,
-//             subreddit_id: comment.subreddit_id
-//           });
-//         });
-//         userProcessed(users.length);
-//       });
-//     });
-//   });
-// }
 
 console.log("Running...");
 
 const threadPromises = [];
-r.getSubreddit('horizon').getHot().forEach((thread) => {
-  threadPromises.push(thread.expandReplies({limit: Infinity, depth: Infinity}));
+// hot threads is a single chainable promise; it has a forEach method though.
+// r.getSubreddit('horizon').getHot()
+
+let counter = 0;
+r.getSubreddit('horizon').getHot().forEach((thread, idx, hotThreads) => {
+  const totalThreads = hotThreads.length;
+  thread.expandReplies({limit: Infinity, depth: Infinity}).then((thread) => {
+    for (let comment of thread.comments) {
+      if (comment.author.name === '[deleted]') continue;
+      if (!userSet[comment.author.name]) userSet[comment.author.name] = {comments:[]};
+    }
+    counter++;
+    if (counter === totalThreads) {
+      processUsers();
+    }
+  }).catch(console.log);
 });
 
-
-
-// async.each(threads, 
-//   (thread) => {
-//     thread.then((thread) => {
-//       thread.comments.forEach((comment) => {
-//         if (!userSet[comment.author.name]) {
-//           userSet[comment.author.name] = {
-//             comments:[]
-//           };
-//         }
-//       });
-//     });
-//   },
-//   (err) => {
-
-//   }
-// );
-
-
-// r.getSubreddit('horizon').getHot().forEach((thread, idx, hotThreads) => {
-//   const totalThreads = hotThreads.length;
-//   thread.expandReplies({limit: Infinity, depth: Infinity}).then((thread) => {
-
-//     thread.comments.forEach((comment) => {
-//       if (!userSet[comment.author.name]) {
-//         userSet[comment.author.name] = {
-//           comments:[]
-//         };
-//       }
-//     });
-//     threadProcessed(totalThreads);
-//   }).catch(console.log);
-// });
-// function check(arg) {
-//   console.log(arg);
-// }
-
-// const prm1 = {
-//   errorState: false,
-//   than:function(callback) {
-//     callback();
-//     return this;
-//   },
-//   catch:function(callback) {
-//     if (this.errorState) callback();
-//   }
-// };
-
-// const prm2 = {
-//   errorState: false,
-//   than:function(callback) {
-//     callback();
-//     return this;
-//   },
-//   catch:function(callback) {
-//     if (this.errorState) callback();
-//   }
-// };
-
-// async.each([prm1,prm2], check, console.log);
+function processUsers() {
+  let counter = 0;
+  const totalUsers = Object.keys(userSet).length;
+  for (let user in userSet) {
+    r.getUser(user).fetch().then(u => {
+      carubsDb.insertUser(u, (err) => {
+        counter++;
+        if (counter === totalUsers) {
+          console.log('Done.');
+        }
+      });
+    }).catch(console.log);
+  }
+}
